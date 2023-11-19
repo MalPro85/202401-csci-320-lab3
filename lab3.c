130cb7583b456f140dbf983a6468f4f9be49fcf2
Malachi Proctor <mkproctor20@students.desu.edu>
2023-11-19 23:55:51 +0000

    properly reading board from file

--- lab3.c
+++ lab3.c
@@ -2,15 +2,35 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include "lab3.h"
+#include <string.h>
 
 extern int** sudoku_board;
 int* worker_validation;
 
-int** read_board_from_file(char* filename){
+int** read_board_from_file(char* filename)
+{
     FILE *fp = NULL;
     int** board = NULL;
-
-    // replace this comment with your code
+    for (int row = 0; row < ROW_SIZE; row++)
+    {
+    	board[row] = (int *)malloc(sizeof(int) * COL_SIZE);
+    }
+    
+    if(fp ==NULL)
+    {
+    	printf("Error: could not open file %s", filename);
+    	return NULL;
+    }
+    
+    for (int i = 0; i < ROW_SIZE; i++)
+    {
+    	for (int j = 0; j < COL_SIZE; j++)
+    	{
+    		fscanf(fp, "%d,", &board[i][j];
+    	}
+    }
+    
+    fclose(fp);
 
     return board;
 }
--- makefile
+++ makefile
@@ -3,10 +3,10 @@ OBJECTS = lab3.o main.o
 validate_sudoku: $(OBJECTS)
 	gcc -o $@ $(OBJECTS) 
 
-lab3.o:lab3.c
+lab3.o:lab3.c lab3.h
 	gcc -c lab3.c
 
-main.o: 
+main.o: main.c lab3.h
 	gcc -c main.c
 
 clean:
